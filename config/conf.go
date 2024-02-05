52f12cd27766e96955732666625b61a2b8d65015
lxy <594328762@qq.com>
2024-02-05 17:26:41 +0800

    bug

--- config/conf.go
+++ config/conf.go
@@ -17,8 +17,6 @@ func ReadConf(key string) interface{} {
 		instance.SetConfigName("config") // 配置文件名称(无扩展名)
 		instance.SetConfigType("yaml")   // 如果配置文件的名称没有扩展名，则需要配置此项
 		instance.AddConfigPath(".")      // 查找配置文件所在的路径
-		instance.AddConfigPath("./")     // 查找配置文件所在的路径
-		instance.AddConfigPath("../")    // 查找配置文件所在的路径
 	})
 	err := instance.ReadInConfig() // 查找并读取配置文件
 	if err != nil {                // 处理读取配置文件的错误
