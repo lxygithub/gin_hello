85d7576b8bbdf243b5c2668cf009c0e4da608ae3
lxy <594328762@qq.com>
2024-02-06 16:21:20 +0800

    bug

--- main.go
+++ main.go
@@ -1,20 +1,15 @@
 package main
 
-import (
-	"fmt"
-	"gin_hello/config"
-	"gin_hello/database"
-)
-
 func main() {
 
-	database.InitDB(fmt.Sprintf("%s:%s@tcp(%s:%d)/%s",
-		config.ReadConf("database.username").(string),
-		config.ReadConf("database.password").(string),
-		config.ReadConf("database.host").(string),
-		config.ReadConf("database.port").(int),
-		config.ReadConf("database.dbname").(string),
-	))
-	_ = GinServer().Run(fmt.Sprintf(":%d", config.ReadConf("server.port").(int)))
+	//database.InitDB(fmt.Sprintf("%s:%s@tcp(%s:%d)/%s",
+	//	config.ReadConf("database.username").(string),
+	//	config.ReadConf("database.password").(string),
+	//	config.ReadConf("database.host").(string),
+	//	config.ReadConf("database.port").(int),
+	//	config.ReadConf("database.dbname").(string),
+	//))
+	//_ = GinServer().Run(fmt.Sprintf(":%d", config.ReadConf("server.port").(int)))
+	_ = GinServer().Run(":8081")
 
 }
