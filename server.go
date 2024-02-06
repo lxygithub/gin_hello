a78774aa26a120c09fc6dbf98dbe7d7e5f73c49d
lxy <594328762@qq.com>
2024-02-06 18:34:29 +0800

    jwt

--- server.go
+++ server.go
@@ -38,5 +38,36 @@ func GinServer() *gin.Engine {
 	ginServer.NoRoute(func(c *gin.Context) {
 		c.Redirect(http.StatusMovedPermanently, "/")
 	})
+
+	ginServer.POST("/login", func(c *gin.Context) {
+		login(c)
+	})
 	return ginServer
+}
+func login(c *gin.Context) {
+	// 创建接收用户登录信息的结构体
+	var loginInfo struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+
+	// 将请求体绑定到结构体
+	if err := c.ShouldBindJSON(&loginInfo); err != nil {
+		c.JSON(http.StatusBadRequest, models.NewErrorResponse(http.StatusBadRequest, "Invalid login parameters"))
+		return
+	}
+
+	// 这里应该通过查询数据库等方法验证用户名和密码的正确性
+	if loginInfo.Username != "admin" || loginInfo.Password != "admin" {
+		c.JSON(http.StatusUnauthorized, models.NewErrorResponse(http.StatusBadRequest, "Authentication failed"))
+		return
+	}
+
+	// 生成JWT令牌，这里只是示例代码，实际应用中应使用安全的方式生成和签名token
+	token := "some.jwt.token"
+	// 返回令牌
+	c.JSON(http.StatusOK, models.NewSuccessResponse(
+		map[string]interface{}{
+			"token": token,
+		}))
 }
