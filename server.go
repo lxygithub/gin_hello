2b4713ac0161df2419eab6497895a57c99322c0f
lxy <594328762@qq.com>
2024-06-11 14:33:44 +0800

    添加登录验证

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"gin_hello/auth"
+	"gin_hello/config"
 	"gin_hello/database"
 	"gin_hello/kimi"
 	"gin_hello/middle_ware"
@@ -43,32 +44,19 @@ func GinServer() *gin.Engine {
 		database.GetUsers(c)
 
 	})
-	/**
-	data class RecordingServiceConfig(
-	    val configUrl: String = "http://117.50.199.110:8081/recording-service-config",
-	    val endpoint: String = "tos-cn-beijing.volces.com",
-	    val region: String = "cn-beijing",
-	    val accessKey: String = "AKLTODNlZTc0N2M4MGMxNGU2ODlmZDRlYTFkYjVlZWIwYTU",
-	    val secretKey: String = "T0dOaU5EUTVNV1JqWlRBeE5EbGpOR0U0TkdVelpqSTFZalprT1RSbE5qRQ==",
-	    val bucketName: String = "ark-auto-created-required-2101485836-cn-beijing",
-	    val maxConcurrentTaskNum: Int = 3,
-	    val audioSliceSize: Int = 10 * 1024 * 1024,
-	    val skipSilence: Boolean = false,
-	)
-	*/
 	ginServer.GET("/recording-service-config", func(c *gin.Context) {
 
 		c.JSON(http.StatusOK, models.NewSuccessResponse(
 			map[string]interface{}{
-				"configUrl":            "http://117.50.199.110:8081/recording-service-config",
-				"endpoint":             "tos-cn-beijing.volces.com",
-				"region":               "cn-beijing",
-				"accessKey":            "AKLTODNlZTc0N2M4MGMxNGU2ODlmZDRlYTFkYjVlZWIwYTU",
-				"secretKey":            "T0dOaU5EUTVNV1JqWlRBeE5EbGpOR0U0TkdVelpqSTFZalprT1RSbE5qRQ==",
-				"bucketName":           "ark-auto-created-required-2101485836-cn-beijing",
-				"maxConcurrentTaskNum": 3,
-				"audioSliceSize":       10 * 1024 * 1024,
-				"skipSilence":          "false",
+				"configUrl":            config.ReadConf("configUrl").(string),
+				"endpoint":             config.ReadConf("endpoint").(string),
+				"region":               config.ReadConf("region").(string),
+				"accessKey":            config.ReadConf("accessKey").(string),
+				"secretKey":            config.ReadConf("secretKey").(string),
+				"bucketName":           config.ReadConf("bucketName").(string),
+				"maxConcurrentTaskNum": config.ReadConf("maxConcurrentTaskNum").(int),
+				"audioSliceSize":       config.ReadConf("audioSliceSize").(int),
+				"skipSilence":          config.ReadConf("skipSilence").(bool),
 			},
 		))
 	})
